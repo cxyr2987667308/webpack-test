6b5484538f900dd5994a85b39d85007307bc1c0e
cxyr2987667308 <lj.fang@zuolin.com>
2018-01-25 16:44:34 +0800

    html-webpack-plugin

---
+++ readme.md
@@ -0,0 +1,15 @@
+### 安装依赖
+npm i
+
+### 启动 json-server
+npm run server
+或者 json-server db.json -w -p 3000
+
+### 启动项目
+npm run dev
+
+### 登录账号、密码
+admin  123456
+
+### 学习心得
+项目是学习了一韬的React实验室的[React全家桶入门],在此学到了很多关于react的的基础知识，引入了Ant Design，同时还学怎么用json server
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,29 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const path = require('path');
+
 module.exports = {
-    entry: './src/script/main.js',
+    entry: {
+        main: './src/script/main.js',
+        a: './src/script/a.js'
+    },
     output: {
-        filename: './dist/js/bundle.js'
-    }
+        path: path.resolve(__dirname, 'dist/js'),
+        filename: '[name]-[chunkhash].js',
+        publicPath: 'http://cdn.com/'  // 线上路径
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin(),
+        new HtmlWebpackPlugin({
+            filename: 'index-[hash].html',
+            template: 'index.html',
+            inject: false, // 'head' 'body', 
+            title: 'webpack is good',
+            date: new Date(),
+            minify: {  // 压缩
+                removeComments: true,  // 删除注释
+                collapseWhitespace: true  // 删除空格
+            }
+        })
+    ]
 }
